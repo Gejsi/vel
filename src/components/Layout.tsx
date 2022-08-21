cb693d784d42db9a251c91270abd7c0087ce34f5
Gejsi <vgejsi@gmail.com>
2022-08-21 19:29:45 +0200

    Removed dialogs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,10 +6,10 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const { data: session } = useSession({ required: true })
 
   return (
-    <main className='min-h-screen drawer drawer-mobile'>
+    <main className='drawer drawer-mobile min-h-screen'>
       <input id='sidebar' type='checkbox' className='drawer-toggle' />
       {/* pt-0 added because Toolbar has pt-4 */}
-      <section className='drawer-content px-4 lg:px-10 py-4 pt-0' role='group'>
+      <section className='drawer-content px-4 py-4 pt-0 lg:px-10' role='group'>
         {children}
       </section>
       <Sidebar session={session} />
--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import { MdAdd } from 'react-icons/md'
 import Toolbar from '../components/Toolbar'
-import useModal from '../hooks/use-modal'
 import type { NextPageWithLayout } from './_app'
 
 const Decks: NextPageWithLayout = () => {
-  const { openModal, closeModal, Modal } = useModal()
-
   return (
     <>
       <Head>
@@ -14,29 +11,13 @@ const Decks: NextPageWithLayout = () => {
       </Head>
 
       <Toolbar title='Decks'>
-        <button className='btn gap-2' onClick={openModal}>
-          <MdAdd className='w-6 h-6' />
+        <button className='btn gap-2'>
+          <MdAdd className='h-6 w-6' />
           Create deck
         </button>
       </Toolbar>
 
-      <Modal>
-        <h3 className='font-bold text-lg'>
-          Congratulations random Internet user!
-        </h3>
-        <p className='py-4 bg-sky-50'>
-          You've been selected for a chance to get one year of subscription to
-          use Wikipedia for free! hit ESC or click outside of me.
-        </p>
-        <div className='modal-action'>
-          <button className='btn btn-ghost' onClick={closeModal}>
-            Close me!
-          </button>
-        </div>
-      </Modal>
-
       <p>Content</p>
-      <div className='w-80 h-80 bg-primary' />
     </>
   )
 }
