e67da272075e683dcdde014096103f466d10625e
Gejsi <vgejsi@gmail.com>
2022-09-04 16:05:33 +0200

    Added support for code blocks (editor node)

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,13 +2,15 @@ import { createBasicElementsPlugin } from '@udecode/plate-basic-elements'
 import { createPlugins, Plate, type TEditableProps } from '@udecode/plate-core'
 import type { FC } from 'react'
 import { twMerge } from 'tailwind-merge'
-import { Blockquote, Heading1, Paragraph } from './Nodes'
+import { Blockquote, CodeBlock, CodeLine, Heading1, Paragraph } from './Nodes'
 
 const plugins = createPlugins([createBasicElementsPlugin()], {
   components: {
     blockquote: Blockquote,
     p: Paragraph,
     h1: Heading1,
+    code_block: CodeBlock,
+    code_line: CodeLine,
   },
 })
 
@@ -25,6 +27,11 @@ const initialValue = [
     type: 'blockquote',
     children: [{ text: 'Lorem ipsum' }],
   },
+  {
+    type: 'code_block',
+    lang: 'javascript',
+    children: [{ type: 'code_line', children: [{ text: 'const a = 2' }] }],
+  },
 ]
 
 const commonEditorClasses = 'min-h-[12rem] p-4 rounded-xl'
@@ -32,33 +39,36 @@ const commonEditorClasses = 'min-h-[12rem] p-4 rounded-xl'
 /**
  * Questions editor props
  */
-const QAttrs: TEditableProps = {
+const QAttributes: TEditableProps = {
   placeholder: 'Question...',
   className: twMerge(commonEditorClasses, 'bg-primary/20'),
+  spellCheck: false,
 }
 
 /**
  * Answers editor props
  */
-const AAttrs: TEditableProps = {
+const AAttributes: TEditableProps = {
   placeholder: 'Answer...',
   className: twMerge(commonEditorClasses, 'bg-secondary/20'),
+  spellCheck: false,
 }
 
 type EditorProps = {
   id: number
 }
 
+// TODO: try wrapping `Plate` with the `prose` className
 const Editor: FC<EditorProps> = ({ id }) => {
   return (
     <div className='mt-4 grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-4'>
       <Plate
         id={`qe-${id}`}
-        editableProps={QAttrs}
+        editableProps={QAttributes}
         plugins={plugins}
         initialValue={initialValue}
       />
-      <Plate id={`ae-${id}`} editableProps={AAttrs} />
+      <Plate id={`ae-${id}`} editableProps={AAttributes} />
     </div>
   )
 }
--- src/components/editor/Nodes.tsx
+++ src/components/editor/Nodes.tsx
@@ -1,34 +1,48 @@
 import type { FC, ReactNode, RefObject } from 'react'
-import { twMerge } from 'tailwind-merge'
 
-type TAttributes<R> = {
+type Attributes<R> = {
   'data-slate-node': 'element'
   ref: RefObject<R>
 }
 
-type Element<T> = {
-  attributes: TAttributes<T>
-  className: string
-  children?: ReactNode
+type Element = {
+  type: string
+  lang?: string
 }
 
-export const Heading1: FC<Element<HTMLHeadingElement>> = (props) => (
-  <h1 {...props.attributes} className={twMerge(props.className, 'text-5xl')}>
+type BlockProps<T> = {
+  attributes: Attributes<T>
+  className: string
+  children?: ReactNode
+  element: Element
+}
+
+export const Heading1: FC<BlockProps<HTMLHeadingElement>> = (props) => (
+  <h1 {...props.attributes} className='text-5xl'>
     {props.children}
   </h1>
 )
 
-export const Paragraph: FC<Element<HTMLParagraphElement>> = (props) => (
-  <p {...props.attributes} className={twMerge(props.className, 'text-lg')}>
+export const Paragraph: FC<BlockProps<HTMLParagraphElement>> = (props) => (
+  <p {...props.attributes} className='text-lg'>
     {props.children}
   </p>
 )
 
-export const Blockquote: FC<Element<HTMLQuoteElement>> = (props) => (
-  <blockquote
-    {...props.attributes}
-    className={twMerge(props.className, 'italic')}
-  >
+export const Blockquote: FC<BlockProps<HTMLQuoteElement>> = (props) => (
+  <blockquote {...props.attributes} className='italic'>
     {props.children}
   </blockquote>
+)
+
+export const CodeBlock: FC<BlockProps<HTMLDivElement>> = (props) => (
+  <div {...props.attributes} className='mockup-code my-2 before:hidden'>
+    {props.children}
+  </div>
+)
+
+export const CodeLine: FC<BlockProps<HTMLPreElement>> = (props) => (
+  <pre {...props.attributes}>
+    <code>{props.children}</code>
+  </pre>
 )
