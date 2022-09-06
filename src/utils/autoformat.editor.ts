e0da0d900a6fa66170ebf51ab570c2758c64219d
Gejsi <vgejsi@gmail.com>
2022-09-06 16:13:50 +0200

    Added marks autoformatting

--- src/utils/autoformat.editor.ts
+++ src/utils/autoformat.editor.ts
@@ -7,6 +7,13 @@ import {
   autoformatSmartQuotes,
 } from '@udecode/plate-autoformat'
 import {
+  MARK_BOLD,
+  MARK_CODE,
+  MARK_ITALIC,
+  MARK_STRIKETHROUGH,
+  MARK_UNDERLINE,
+} from '@udecode/plate-basic-marks'
+import {
   ELEMENT_CODE_BLOCK,
   insertEmptyCodeBlock,
 } from '@udecode/plate-code-block'
@@ -88,8 +95,62 @@ const autoformatBlocks = [
   },
 ]
 
+export const autoformatMarks = [
+  {
+    mode: 'mark',
+    type: [MARK_BOLD, MARK_ITALIC],
+    match: '***',
+  },
+  {
+    mode: 'mark',
+    type: [MARK_UNDERLINE, MARK_ITALIC],
+    match: '__*',
+  },
+  {
+    mode: 'mark',
+    type: [MARK_UNDERLINE, MARK_BOLD],
+    match: '__**',
+  },
+  {
+    mode: 'mark',
+    type: [MARK_UNDERLINE, MARK_BOLD, MARK_ITALIC],
+    match: '___***',
+  },
+  {
+    mode: 'mark',
+    type: MARK_BOLD,
+    match: '**',
+  },
+  {
+    mode: 'mark',
+    type: MARK_UNDERLINE,
+    match: '__',
+  },
+  {
+    mode: 'mark',
+    type: MARK_ITALIC,
+    match: '*',
+  },
+  {
+    mode: 'mark',
+    type: MARK_ITALIC,
+    match: '_',
+  },
+  {
+    mode: 'mark',
+    type: MARK_STRIKETHROUGH,
+    match: '~~',
+  },
+  {
+    mode: 'mark',
+    type: MARK_CODE,
+    match: '`',
+  },
+]
+
 const autoformatRules = [
   ...autoformatBlocks,
+  ...autoformatMarks,
   ...autoformatSmartQuotes,
   ...autoformatPunctuation,
   ...autoformatLegal,
